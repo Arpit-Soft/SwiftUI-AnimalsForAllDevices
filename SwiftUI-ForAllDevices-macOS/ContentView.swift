fd0413516e949b48ab0db7c247e1a3a7e2f6467a
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-05 16:53:27 +0530

    Added macos Views

--- SwiftUI-ForAllDevices-macOS/ContentView.swift
+++ SwiftUI-ForAllDevices-macOS/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedAnimal: Animal? = nil
+
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        NavigationView {
+            GeometryReader { g in
+                AnimalList(selectedAnimal: $selectedAnimal)
+            }
+            GeometryReader { g in
+                SharedDetailView(animal: selectedAnimal ?? Animal.placeholder)
+            }
+            
+        }
+        .navigationViewStyle(DoubleColumnNavigationViewStyle())
     }
 }
 
@@ -18,5 +29,25 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AnimalList: View {
+    
+    let animals = AnimalService.getAll()
+    @Binding var selectedAnimal: Animal?
+    
+    var body: some View {
+        List(animals, id: \.name) { animal in
+            VStack {
+                Text(animal.image)
+                    .font(.custom("Arial", size: 90))
+                Text(animal.name)
+                    .font(.title)
+            }
+            .onTapGesture {
+                selectedAnimal = animal
+            }
+        }
     }
 }
